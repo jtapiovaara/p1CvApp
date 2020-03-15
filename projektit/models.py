b9ce1b16eb768a68e1a4663cacbbdf9834e432ff
jta <jtapiovaara@gmail.com>
2020-03-15 13:47:15 +0200

    2nd  Commit

--- projektit/models.py
+++ projektit/models.py
@@ -5,8 +5,12 @@ class Projekti(models.Model):
     title = models.CharField(max_length=32, blank=True)
     shortdescription = models.TextField()
     description = models.TextField(blank=True)
+    app_image = models.ImageField(blank=True)
     image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ('shortdescription',)
 
